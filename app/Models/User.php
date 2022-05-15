b48b4e38c0d2cc25d3fe3cc0b762d085052d26fe
Bengoufa Abdallah <bengoufabem@gmail.com>
2022-05-15 20:41:55 +0100

    idk

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,11 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Laratrust\Traits\LaratrustUserTrait;
+
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-
+    use LaratrustUserTrait;
     /**
      * The attributes that are mass assignable.
      *
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -54,48 +54,51 @@
 <!--Navbar-->
 <nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3">
     <div class="container">
-      <a href="/" class="navbar-brand"> Find My Med</a>
-    <button class="navbar-toggler" type="button"
-    data-bs-toggle="collapse" data-bs-target="#navmenu">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="#navmenu">
-      <ul class="navbar-nav ms-auto">
-        <li class="nav-item">
-          <a href="/" class="nav-link">A propos</a>
-        </li>
-        @guest
-           @if (Route::has('login'))
-              <li class="nav-item">
-              <a class="nav-link" href="{{ route('login') }}">Se Connecter</a>
-              </li>
-          @endif
+        <a href="/" class="navbar-brand"> Find My Med</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="#navmenu">
+            <ul class="navbar-nav ms-auto">
+                {{-- <li class="nav-item">
+                    <a href="/" class="nav-link">A propos</a>
+                </li> --}}
+                @guest
+                    @if (Route::has('login'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">Se Connecter</a>
+                        </li>
+                    @endif
 
-           @if (Route::has('register'))
-              <li class="nav-item">
-              <a class="nav-link" href="{{ route('register') }}">S'inscrire</a>
-              </li>
-           @endif
-        @else  
-        <li class="nav-item dropdown">
-            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                {{ Auth::user()->name }}
-            </a>
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">S'inscrire</a>
+                        </li>
+                    @endif
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
 
-            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="{{ route('logout') }}"
-                   onclick="event.preventDefault();
-                                 document.getElementById('logout-form').submit();">
-                    {{ __('Logout') }}
-                </a>
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+                            <a class="dropdown-item" href="{{ route('dashboard.profile') }}" onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                                {{ __('Mon profile') }}
+                            </a>
 
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                    @csrf
-                </form>
-            </div>
-        </li>
-        @endguest
-      </ul>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
+        </div>
     </div>
-    </div>
-  </nav>
+</nav>
