75e9cc7dc9cb1779cb40822040cfb695b8a89b12
Bengoufa Abdallah <bengoufabem@gmail.com>
2022-05-18 03:04:34 +0100

    livewire

--- resources/views/layouts/appbt.blade.php
+++ resources/views/layouts/appbt.blade.php
@@ -34,6 +34,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    @livewireStyles
 </head>
 
 <body>
@@ -52,6 +53,7 @@
         </footer>
 
     </div>
+    @livewireScripts
 </body>
 
 </html>
