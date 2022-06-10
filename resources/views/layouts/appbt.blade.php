a1b983d24755e7797e83a8c76d1d1a11c5b2b9a4
Bengoufa Abdallah <bengoufabem@gmail.com>
2022-06-10 23:10:46 +0100

    phph

--- resources/views/layouts/appbt.blade.php
+++ resources/views/layouts/appbt.blade.php
@@ -34,7 +34,7 @@
     <!-- Styles -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link href="{{ asset('css/mdb.min.css') }}" rel="stylesheet">
+    <!-- <link href="{{ asset('css/mdb.min.css') }}" rel="stylesheet"> -->
     <link rel="stylesheet"
         href="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.14.1/css/ol.css" type="text/css">
     <style>
