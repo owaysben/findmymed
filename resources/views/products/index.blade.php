c397c00b10626b89b510a0dcb0fa24c102de3155
IhceneAmr01 <a9.ihcene@gmail.com>
2022-06-15 06:33:00 +0200

    fixed results UI

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -11,52 +11,131 @@
             </div>
         </div>
     </section>
-    <div class="container justify-items-center">
-   <div class="card" style="width: 95%;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-        <h5 class="card-title">Nom médicament</h5>
-        <p class="card-text">
-            forme: smth <br>
-            Prix: default
-        </p>
-    </div>
+    <div class="container justify-items-center text-align-center">
+    <section class="d-flex d-md-flex justify-items-center text-align-center">
+
+        <div class="container">
+            <div class="bg-white text-dark">
+                <div class=" text-center">
+                    <div>
+                        <img src="\images\ph2.jpg" alt="med" style="max-height:45%; max-width:45%">
+                    </div>
+
+                        <h3 class="title mb-3">Nom Médoc</h3>
+                        <p class="text">
+                            Forme: smth.<br>
+                            price: smth.
+                        </p>
+                </div>
+            </div>
+        </div>
+    </section>
+   <div class="card justify-items-center text-align-center mb-25"style="width: 100%;">
+
+
     <ul class="list-group list-group-flush">
-        <li class="list-group-item">Pharmacy name(+) </li>
-        <li class="list-group-item">Pharmacy name(+)</li>
-        <li class="list-group-item">Pharmacy name(+)</li>
+        <li class="list-group-item shadow">
+            <h5>Pharmacy name(+)</h5>
+            <h6 class="fw-light">Adresse:@@@@@@@</h6>
+            <div class="float-end">
+            <button class="btn bt-md btn-primary justify-content-md-center" type="modal">
+                Afficher localisation
+            </button>
+             <button class="btn bt-md btn-success justify-content-md-center" type="modal">
+                Réserver
+            </button>
+            </div>
+        </li>
+        <li class="list-group-item shadow">
+            <h5>Pharmacy name(+)</h5>
+            <h6 class="fw-light">Adresse:@@@@@@@</h6>
+            <div class="float-end">
+            <button class="btn bt-md btn-primary justify-content-md-center" type="modal">
+                Afficher localisation
+            </button>
+             <button class="btn bt-md btn-success justify-content-md-center" type="modal">
+                Réserver
+            </button>
+            </div>
+        </li>
+        <li class="list-group-item shadow">
+            <h5>Pharmacy name(+)</h5>
+            <h6 class="fw-light">Adresse:@@@@@@@</h6>
+            <div class="float-end">
+            <button class="btn bt-md btn-primary justify-content-md-center" type="modal">
+                Afficher localisation
+            </button>
+             <button class="btn bt-md btn-success justify-content-md-center" type="modal">
+                Réserver
+            </button>
+            </div>
+        </li>
     </ul>
-    <div class="card-body">
-        <a href="#" class="card-link">hna dji hadik tae <1....10></a>
+    <div class="card-body ">
+        <a href="#" class="card-link ">hna dji hadik tae <1....10></a>
 
     </div>
 </div>
-    </div>
-     <section class="bg-light text-dark p-2">
-        <div class="container ">
+</div>
+     <section class="bg-white text-dark p-2">
+        <div class="container px-5 mb-15">
             <div class="d-md-flex justify-content-between align-items-center position-relative">
-                <h3 class=>Vous n'arrivez pas à trouver une pharmacie proche de vous?
+                <h3 class="mb-4 mt-4 text-secondary">Vous n'arrivez pas à trouver une pharmacie proche de vous?
                 </h3> <br>
 
             </div>
-            <h6>Essayer de faire une commande chez l'une des pharmacies suivantes!</h6>
+            <h5 class="mb-4 fw-light "> Essayer de faire une commande chez l'une des pharmacies suivantes!</h5>
+
         </div>
     </section>
     <div class="container justify-items-center">
-   <div class="card" style="width: 95%;">
+   <div class="card" style="width: 100%;">
 
 
     <ul class="list-group list-group-flush">
-        <li class="list-group-item">Pharmacy name(-) </li>
-        <li class="list-group-item">Pharmacy name(-)</li>
-        <li class="list-group-item">Pharmacy name(-)</li>
+        <li class="list-group-item shadow">
+           <h5>Pharmacy name(+)</h5>
+            <h6 class="fw-light">Adresse:@@@@@@@</h6>
+            <div class="float-end">
+            <button class="btn bt-md btn-primary justify-content-md-center" type="modal">
+                Afficher localisation
+            </button>
+             <button class="btn bt-md btn-success justify-content-md-center" type="modal">
+                Commander
+            </button>
+            </div>
+        </li>
+        <li class="list-group-item shadow">
+            <h5>Pharmacy name(+)</h5>
+            <h6 class="fw-light">Adresse:@@@@@@@</h6>
+            <div class="float-end">
+            <button class="btn bt-md btn-primary justify-content-md-center" type="modal">
+                Afficher localisation
+            </button>
+             <button class="btn bt-md btn-success justify-content-md-center" type="modal">
+                Commander
+            </button>
+            </div>
+        </li>
+        <li class="list-group-item shadow"><h5>Pharmacy name(+)</h5>
+            <h6 class="fw-light">Adresse:@@@@@@@</h6>
+            <div class="float-end">
+            <button class="btn bt-md btn-primary justify-content-md-center" type="modal">
+                Afficher localisation
+            </button>
+             <button class="btn bt-md btn-success justify-content-md-center" type="modal">
+                Commander
+            </button>
+            </div>
+        </li>
     </ul>
-    <div class="card-body">
+    <div class="card-body ">
         <a href="#" class="card-link">hna dji hadik tae <1....10></a>
 
     </div>
 </div>
     </div>
+    <div class="container mx-20 my-20">
  {{--  <div class="card bg-light text-dark">
                         <div class="card-body text-center">
 
