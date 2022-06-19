2f771c7b37b6cf60cc85e3217efa09066a1f7297
Bengoufa Abdallah <bengoufabem@gmail.com>
2022-06-19 01:27:15 +0100

    1

--- resources/views/layouts/appbt.blade.php
+++ resources/views/layouts/appbt.blade.php
@@ -13,7 +13,8 @@
     <!-- Scripts -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.14.1/build/ol.js"></script>
-    <script src="{{ asset('js/select2.js') }}" defer></script>
+    {{-- <script src="{{ asset('js/select2.js') }}" defer></script> --}}
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
